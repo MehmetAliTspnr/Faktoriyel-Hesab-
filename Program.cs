067ebbcd2ab1104b5ee74f33b87d470939a209d5
Mehmet Ali <101504173+MehmetAliTspnr@users.noreply.github.com>
2022-07-17 21:28:57 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Faktoriyel_Örnek
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i, faktor, deger;
+            faktor = 1;
+            Console.WriteLine("Faktoriyel Alinacak Sayiyi Giriniz:");
+            deger = Convert.ToInt32(Console.ReadLine());
+           
+            while(deger>1)
+            {
+
+                faktor = faktor * deger;
+                deger--;
+
+            }
+            Console.WriteLine(faktor);
+
+            Console.Read();
+        }
+    }
+}
